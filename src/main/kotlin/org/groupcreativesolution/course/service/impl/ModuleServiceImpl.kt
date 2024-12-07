20ca238e91c174d4316aef398004173e1f144f00
Jose Augusto <jose.malengo@myp7.com.br>
2024-12-07 17:19:12 -0300

    create service

--- src/main/kotlin/org/groupcreativesolution/course/service/impl/ModuleServiceImpl.kt
+++ src/main/kotlin/org/groupcreativesolution/course/service/impl/ModuleServiceImpl.kt
@@ -3,6 +3,8 @@ package org.groupcreativesolution.course.service.impl
 import org.groupcreativesolution.course.repositories.ModuleRepository
 import org.groupcreativesolution.course.service.ModuleService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 
-class ModuleServiceImpl(@Autowired var repository: ModuleRepository ): ModuleService {
+@Service
+class ModuleServiceImpl(@Autowired private val repository: ModuleRepository ): ModuleService {
 }
