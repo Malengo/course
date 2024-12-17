2eab90d1ed6db20b7513358eaf924d1d8df035aa
Jose Augusto <jose.malengo@myp7.com.br>
2024-12-17 16:53:10 -0300

    create test for to valid field

--- src/main/kotlin/org/groupcreativesolution/course/exception/ValidationFieldExceptionHandler.kt
+++ src/main/kotlin/org/groupcreativesolution/course/exception/ValidationFieldExceptionHandler.kt
@@ -2,6 +2,7 @@ package org.groupcreativesolution.course.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -15,5 +16,10 @@ class ValidationFieldExceptionHandler {
             it.field to (it.defaultMessage ?: "")
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(ex: HttpMessageNotReadableException): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid JSON request")
     }
 }
